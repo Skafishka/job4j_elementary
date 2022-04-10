59e8c6648479236958c31489ac75ae49fd10fbce
Skafishka <sergey.kolygin@gmail.com>
2022-04-10 23:04:58 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,8 +6,12 @@ public class Max {
         return result;
     }
 
+    public static int max(int left1, int right1, int left, int right) {
+        return max(left1, max(right1, max(left, right)));
+    }
+
     public static void main(String[] args) {
-        int rsl = Max.max(3, 1);
+        int rsl = Max.max(3, 1, 4, 7);
         System.out.println(rsl);
     }
 
---
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaxTest {
+    @Test
+    public void whenMax1To7Then7() {
+        int left1 = 3;
+        int right1 = 1;
+        int left = 4;
+        int right = 7;
+        int result = Max.max(left1, right1, left, right);
+        int expected = 7;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax39to1179Then1179() {
+        int left1 = 39;
+        int right1 = 69;
+        int left = 1179;
+        int right = 555;
+        int result = Max.max(left1, right1, left, right);
+        int expected = 1179;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax5to99Then99() {
+        int left1 = 0;
+        int right1 = 40;
+        int left = 33;
+        int right = 99;
+        int result = Max.max(left1, right1, left, right);
+        int expected = 99;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax8to8Then8() {
+        int left1 = 8;
+        int right1 = 8;
+        int left = 8;
+        int right = 8;
+        int result = Max.max(left1, right1, left, right);
+        int expected = 8;
+        Assert.assertEquals(result, expected);
+    }
+}
